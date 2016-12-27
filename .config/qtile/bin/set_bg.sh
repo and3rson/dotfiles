a9650472722df9400eaedcf77e61f707a78250f2
Andrew Dunai <andunai@gmail.com>
2016-12-27 02:55:49 +0200

    Fix

--- .config/qtile/bin/set_bg.sh
+++ .config/qtile/bin/set_bg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo bg set1
-feh --bg-fill ~/.wallpapers/new/zbFCJbj.jpg
+# feh --bg-fill ~/.wallpapers/new/zbFCJbj.jpg
+# feh --bg-fill ~/.wallpapers/new/86ffb87572d657f335cd7cd828c70de3.jpg
+feh --bg-fill ~/.wallpapers/new2/nature-background-vector-apple-wallpaper-night-city-3.jpg
 echo bg set2
