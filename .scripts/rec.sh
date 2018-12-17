0051287cb658bbbb2c47f826d0623360e2ef3542
Andrew Dunai <a@dun.ai>
2018-12-17 16:35:14 +0200

    Scripts.

--- .scripts/rec.sh
+++ .scripts/rec.sh
@@ -12,5 +12,7 @@ FNAME_MP4="$FNAME_NO_EXT.mp4"
 VARS=(`xmeasure 2>&1 | awk -F': ' '{ print $2 }'`)
 read X Y W H <<< "${VARS[*]}"
 
-recordmydesktop --no-sound -x $X -y $Y --width $W -height $H --fps 30 --overwrite -o $FNAME_OGV
+CMD="recordmydesktop --no-sound -x $X -y $Y --width $W -height $H --fps 30 --overwrite -o $FNAME_OGV --stop-shortcut Control+s"
+echo $CMD
+$CMD
 ffmpeg -y -i $FNAME_OGV $FNAME_MP4
