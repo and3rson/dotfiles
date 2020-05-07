e84edda510f3cbf6f1b77ae16874373e697831af
Andrew Dunai <a@dun.ai>
2020-05-07 19:29:34 +0300

    Awesomewm df widget

--- .config/awesome/widgets/df.lua
+++ .config/awesome/widgets/df.lua
@@ -4,44 +4,43 @@ local beautiful = require("beautiful")
 
 local CMD = [[stat -f / --format "%b %a %s"]]
 
-local progressbar = wibox.widget {
-    forced_width=10,
-    max_value=100,
-    value=0,
-    background_color='#44FF77',
-    color=beautiful.bg_focus,
-    widget=wibox.widget.progressbar,
-    margins={
-        bottom=15
+return function(s)
+    local progressbar = wibox.widget {
+        forced_width=10,
+        max_value=100,
+        value=0,
+        background_color=beautiful.bg_weak,
+        color=beautiful.bg_focus,
+        widget=wibox.widget.progressbar,
+        margins=beautiful.progressbar_margins
     }
-}
 
-local df_widget = wibox.widget{
-    paddings=2,
-    markup='~',
-    widget=wibox.widget.textbox
-}
+    local df_widget = wibox.widget{
+        paddings=2,
+        markup='~',
+        widget=wibox.widget.textbox
+    }
 
-local update_widget = function(widget, stdout, _, _, _)
-    local parts = stdout:gmatch('%S+')
-    local blocks_total = tonumber(parts())
-    local blocks_free = tonumber(parts())
-    local block_size = tonumber(parts())
+    local update_widget = function(widget, stdout, _, _, _)
+        local parts = stdout:gmatch('%S+')
+        local blocks_total = tonumber(parts())
+        local blocks_free = tonumber(parts())
+        local block_size = tonumber(parts())
 
-    local percentage = (1 - blocks_free / blocks_total) * 100
+        local percentage = (1 - blocks_free / blocks_total) * 100
 
-    widget.markup = string.format(
-        '%.1f',
-        block_size * blocks_free / (1024 ^ 3)
-    ) .. ' GB'
-    progressbar.value = percentage
+        widget.markup = string.format(
+            '%.1f',
+            block_size * blocks_free / (1024 ^ 3)
+        ) .. ' GB'
+        progressbar.value = percentage
+    end
+
+    watch(CMD, 10, update_widget, df_widget)
+
+    return wibox.widget{
+        progressbar,
+        df_widget,
+        layout=wibox.layout.stack
+    }
 end
-
-watch(CMD, 10, update_widget, df_widget)
-
-return wibox.widget{
-    progressbar,
-    df_widget,
-    layout=wibox.layout.stack
-}
-
