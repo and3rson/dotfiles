aaab2a9a5dea0e8ace813581bd43d10e76ea860e
Andrew Dunai <a@dun.ai>
2020-05-15 01:31:36 +0300

    Calibration scripts.

---
+++ .scripts/calibrate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DISPLAY=:0
+
+xinput set-prop 'Silead GSLx680 Touchscreen' 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
+
---
+++ .scripts/calibrate_right_up.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DISPLAY=:0
+
+xinput set-prop 'Silead GSLx680 Touchscreen' 'Coordinate Transformation Matrix' 0 1 0 -1 0 1 0 0 1
+
